d8a0c277bd80e5e6411834ffce28fbf5ba814df9
Saiiu7126900 <Sakorn.Nil@wdc.com>
2025-04-18 14:30:17 +0700

    Add files via upload

---
+++ Teachable-ME1-CuteTheme/docs/assets/script.js
@@ -0,0 +1,5 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const sound = document.getElementById('alertSound');
+    setTimeout(() => sound.play(), 1000);
+});
---
+++ Teachable-ME1-CuteTheme/docs/script.js
@@ -0,0 +1,1 @@
+// JS logic for posture detection with Firebase and Chart.js
